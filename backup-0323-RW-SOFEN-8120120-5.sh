eedbe1e59acd2aa6a8d48ff71e9a870e92b3ffd3
JesseKiguta <j.kiguta@alustudent.com>
2023-06-24 10:53:30 -0700

    made changes during review

--- backup-0323-RW-SOFEN-8120120-5.sh
+++ backup-0323-RW-SOFEN-8120120-5.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Creates shell script to back up the directory in part a
 host="a0dfc11ba139.a982e858.alu-cod.online"
 user="a0dfc11ba139"
-pwd="899c79fc532cd0591af1"
-source="0323-RW-SOFEN-8120120-5_q1"
-dstn="/home/sftp-student/03033/summative"
-sshpass -p $pwd scp -r $source $user@$host:$dstn
+password="899c79fc532cd0591af1"
+directory="0323-RW-SOFEN-8120120-5_q1/"
+backup_location="/home/sftp-student/03033/summative"
+sshpass -p $password scp -r $directory $user@$host:$backup_location
